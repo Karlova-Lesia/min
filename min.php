1c76ff3059f9570607aeada80a29cf6f041b7917
Karlova-Lesia <74463878+Karlova-Lesia@users.noreply.github.com>
2021-05-19 22:55:02 +0300

    Add files via upload

--- min.php
+++ min.php
@@ -3,19 +3,18 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-  function minimum(array $numbers)
+  function minimum(array $numbers): array
   {
     $arrayMinNumbers = [];
 
     $minFirst = min(array_values($numbers));
     $arrayMinNumbers[] = $minFirst;
-    unset($minFirst, $nunbers);
     $numberWithoutFirstMin = array_diff($numbers, $arrayMinNumbers);
     $minSecond = min(array_values($numberWithoutFirstMin));
     $arrayMinNumbers[] = $minSecond;
-    
-    print_r($arrayMinNumbers);
+
+    return $arrayMinNumbers;
   }
 
   $num = [3, -4, 2, 3, -1, 2];
-  minimum($num);
+  print_r(minimum($num));
