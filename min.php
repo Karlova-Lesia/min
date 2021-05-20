3d96bac2240e3cab3cb236b41e9cf4eda4bd2ec8
Karlova-Lesia <74463878+Karlova-Lesia@users.noreply.github.com>
2021-05-20 16:42:24 +0300

    Add files via upload

---
+++ min.php
@@ -0,0 +1,22 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+  function minimum(array $numbers): array
+  {
+    $arrayMinNumbers = [];
+
+    foreach ($numbers as $key => $value) {
+      $minFirst = min(array_values($numbers));
+      $arrayMinNumbers[] = $minFirst;
+      $minFirstKey = array_search($minFirst, $numbers);
+      unset($numbers[$minFirstKey]);
+      $minSecond = min(array_values($numbers));
+      $arrayMinNumbers[] = $minSecond;
+      return $arrayMinNumbers;
+    }
+  }
+
+  $num = [3, -4, 2, 3, -1, 2];
+  var_dump(minimum($num));
